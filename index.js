3c31d5d83f1409cc51e0b03d7c220994fd1de783
dhuw <hazlewood.dyl@gmail.com>
2022-09-01 14:42:49 -0700

    update

--- index.js
+++ index.js
@@ -1,34 +1,79 @@
- const inquirer = require('inquirer');
- const fs = require('fs');
- const employees = [];
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+const Manager = require("./lib/manager");
 
- function initIndex() {
+const employees = [];
 
- }
+function initApp() {
+    addMember();
+}
 
- function addMember() {
+function addMember() {
+    inquirer.prompt([{
+        message: "Enter team member's name",
+        name: "name"
+    },
+    {
+        type: "list",
+        message: "Select team member's role",
+        choices: [
+            "Engineer",
+            "Intern",
+            "Manager"
+        ],
+        name: "role"
+    },
+    {
+        message: "Enter team member's id",
+        name: "id"
+    },
+    {
+        message: "Enter team member's email address",
+        name: "email"
+    }])
+    .then(function({name, role, id, email}) {
+        let roleInfo = "";
+        if (role === "Engineer") {
+            roleInfo = "GitHub username";
+        } else if (role === "Intern") {
+            roleInfo = "school name";
+        } else {
+            roleInfo = "office phone number";
+        }
         inquirer.prompt([{
-            message: "What is the team members name?",
-            name: "name"
+            message: `Enter team member's ${roleInfo}`,
+            name: "roleInfo"
         },
         {
             type: "list",
-            message: "Select members position:",
+            message: "Would you like to add more team members?",
             choices: [
-                "Engineer",
-                "Intern",
-                "Manager"
+                "yes",
+                "no"
             ],
-            name: "role"
-        },
-        {
-            message: "Enter members ID:",
-            name: "id"
-        },
-        {
-            message: "Enter members email address",
-            name: "email"
-        }
-        ])
-    }
-
+            name: "moreMembers"
+        }])
+        .then(function({roleInfo, moreMembers}) {
+            let newMember;
+            if (role === "Engineer") {
+                newMember = new Engineer(name, id, email, roleInfo);
+            } else if (role === "Intern") {
+                newMember = new Intern(name, id, email, roleInfo);
+            } else {
+                newMember = new Manager(name, id, email, roleInfo);
+            }
+            employees.push(newMember);
+            addHtml(newMember)
+            .then(function() {
+                if (moreMembers === "yes") {
+                    addMember();
+                } else {
+                    finishHtml();
+                }
+            });
+            
+        });
+    });
+}
